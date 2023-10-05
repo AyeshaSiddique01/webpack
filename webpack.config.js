63597f649a4445645d3cbc70d2502944cd4fbb0b
Ayesha13068 <ayesha.siddique@arbisoft.com>
2023-10-05 12:33:59 +0500

    feat: Cleaning up the /dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,19 @@
-const path = require('path');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
- module.exports = {
+module.exports = {
   entry: {
-    index: './src/index.js',
-    print: './src/print.js',
+    index: "./src/index.js",
+    print: "./src/print.js",
   },
-   output: {
-    filename: '[name].bundle.js',
-     path: path.resolve(__dirname, 'dist'),
-   },
- };
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Output Management",
+    }),
+  ],
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+};
